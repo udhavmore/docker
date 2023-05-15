4db978a9af54b829a6cfb7cf02db038633975a13
taura5 <udhav.tauras04@gmail.com>
2023-05-15 13:33:50 +0530

    Adding docker based Frontend JavaScript Project

---
+++ frontend/src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// New test for checking if docker run tests on new test (ep 83.Docker Compose for running tests)
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
